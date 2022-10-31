fa97ad44a3effd175f252c9ae8d41f770ab38f66
Svetlyo <svetlyo2@gmail.com>
2022-10-31 22:53:05 +0200

    added solution 5

---
+++ Java OOP/07. Reflection and Annotations/Lab/CodingTracker/Tracker.java
@@ -0,0 +1,17 @@
+package CodingTracker;
+
+public class Tracker {
+    @Author(name = "Pesho")
+    public void test(){
+    }
+
+    @Author(name = "Gosho")
+    private static void methodByGosho(){
+    }
+
+    @Author(name = "Misho")
+    public String test2(){
+        return "Test";
+    }
+
+}
