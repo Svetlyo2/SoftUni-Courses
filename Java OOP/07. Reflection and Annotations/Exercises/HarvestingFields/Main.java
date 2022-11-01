921345ca6c21d39e892bbca3b986562bdb6db573
Svetlyo <svetlyo2@gmail.com>
2022-11-01 23:24:58 +0200

    Initial commit

---
+++ Java OOP/07. Reflection and Annotations/Exercises/HarvestingFields/Main.java
@@ -0,0 +1,32 @@
+package HarvestingFields;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Field[] fields = RichSoilLand.class.getDeclaredFields();
+        Consumer<Field> printer = f -> System.out.printf("%s %s %s%n", Modifier.toString(f.getModifiers()),
+                f.getType().getSimpleName(), f.getName());
+        while (!input.equalsIgnoreCase("harvest")) {
+            String finalInput = input;
+
+            Field[] requestedFields = Arrays.stream(fields)
+                    .filter(f -> Modifier.toString(f.getModifiers()).equalsIgnoreCase(finalInput))
+                    .toArray(Field[]::new);
+
+            if (requestedFields.length != 0) {
+                Arrays.stream(requestedFields).forEach(printer);
+            } else {
+                Arrays.stream(fields).forEach(printer);
+            }
+            input = scanner.nextLine();
+        }
+    }
+}
