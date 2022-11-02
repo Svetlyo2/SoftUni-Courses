ae4008001c618672748ddcaf2b2f9ba5c12855b3
Svetlyo <svetlyo2@gmail.com>
2022-11-02 22:50:27 +0200

    added solution 2

---
+++ Java OOP/07. Reflection and Annotations/Exercises/BlackBoxInteger/Main.java
@@ -0,0 +1,36 @@
+package BlackBoxInteger;
+
+import BlackBoxInteger.com.BlackBoxInt;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Constructor<BlackBoxInt> constructor = BlackBoxInt.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        BlackBoxInt blackBoxInt = constructor.newInstance();
+        Method[] methods = blackBoxInt.getClass().getDeclaredMethods();
+        Field innerValue = blackBoxInt.getClass().getDeclaredField("innerValue");
+        innerValue.setAccessible(true);
+
+        while (!input.equalsIgnoreCase("end")) {
+            String[] tokens = input.split("_");
+            String command = tokens[0];
+            int value = Integer.parseInt(tokens[1]);
+            Method method = Arrays.stream(methods)
+                    .filter(m -> m.getName().equalsIgnoreCase(command)).findFirst().orElse(null);
+            method.setAccessible(true);
+            method.invoke(blackBoxInt, value);
+            System.out.println(innerValue.get(blackBoxInt));
+            input = scanner.nextLine();
+        }
+    }
+}
