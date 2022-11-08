3010978932f76ca40d81de0af78646267066558e
Svetlyo <svetlyo2@gmail.com>
2022-11-08 23:59:29 +0200

    added solution 3

---
+++ Java OOP/08. UnitTesting/src/test/java/IteratorTests.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import p03_IteratorTest.ListIterator;
+
+import javax.naming.OperationNotSupportedException;
+
+public class IteratorTests {
+    String elements;
+    ListIterator iterator;
+
+    @Test
+    public void cantCreateInstanceWithNullElements() throws OperationNotSupportedException{
+        ListIterator listIterator = new ListIterator(elements);
+    }
+    @Before
+    public void beforeEach() throws OperationNotSupportedException {
+        iterator = new ListIterator("Stefcho", "Goshky");
+    }
+
+    @Test
+    public void canCreateListIteratorInstance() {
+        Assert.assertEquals(iterator.getClass().getSimpleName(), "ListIterator");
+    }
+
+    @Test
+    public void canMoveIndexToNext() {
+        Assert.assertTrue(iterator.move());
+        Assert.assertEquals(iterator.print(), "Goshky");
+    }
+    @Test
+    public void canMoveReturnsFalseWhenHasNotNextElement() {
+        Assert.assertTrue(iterator.move());
+        Assert.assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void hasNextReturnsTrueWhenHasNextElement() {
+        Assert.assertTrue(iterator.hasNext());
+    }
+
+    @Test
+    public void printReturnsElementAtCurrentIndex() {
+        Assert.assertEquals(iterator.print(), "Stefcho");
+    }
+}
